d69720cffdc7d8c37764a4fc0e8ace776676e4c8
jrjcsj@gmail.com <jrjcsj@gmail.com@88594651-9b5a-8c86-ba1c-9ad1f0a5a5d4>
2013-12-01 03:32:05 +0000

    Sprint 1 - alteração no arquivo LoginActivity.java

--- trunk/src/com/example/busup/LoginActivity.java
+++ trunk/src/com/example/busup/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.busup;
 
 
+import db.Usuario;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.*;
 import android.widget.*;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.app.AlertDialog;
@@ -12,17 +14,22 @@ import android.app.AlertDialog;
 
 public class LoginActivity extends Activity {
 	
-	TextView tvSenhaLogin, tvLogin, tvAcessoRedes;
+	Cursor cursor;
+	SQLiteDatabase database;
+	Usuario usuario = new Usuario();
 	EditText etLogin, etSenhaLogin;
 	Button btEntrar;
 	CheckedTextView ctvCadastrar;
 	String login, senha;
+   // SQL CSQLiteDatabase.CursorFactory()
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		abreBanco();
+	    criaTabelas();
 		
 		
 		etLogin = (EditText) findViewById(R.id.etLogin);
@@ -34,15 +41,11 @@ public class LoginActivity extends Activity {
 			
 			public void onClick (View v) {
 				
-				login = String.valueOf(etLogin.getText());
-				senha =  String.valueOf(etSenhaLogin.getText());
-				
-			
-				
-			}
+				mensagemExibir("teste", "O clique ta funcionando");
+				logar();
 			
 			
-			
+			}		
 		});
 		 
 		 
@@ -55,16 +58,16 @@ public class LoginActivity extends Activity {
 			 
 		 });
 		
+		}
+		
+		
 		
 	
-		
-		Cursor cursor;
-		
-		
-	}
 	
 	public void chamarCadastro() {
-		setContentView(R.layout.activity_cadastro);
+		Intent intent = new Intent(this, CadastroActivity.class);
+		startActivity(intent);
+		
 			 
 		
 	}
@@ -80,16 +83,72 @@ public class LoginActivity extends Activity {
 	}
 	
 	
+	public boolean logar () {
+		
+		try{
+		login = etLogin.getText().toString();
+		senha = etSenhaLogin.getText().toString();
+		
+		cursor = database.query("usuario", 
+			     new String [] {"nomeUsuario","emailUsuario","loginUsuario","senhaUsuario"},
+			     null,
+			     null, 
+				 null, 
+				 null,
+				 null);
+		
+		
+			 
+			 if(cursor.getColumnName(2).intern().equals(login) && cursor.getColumnName(3).intern().equals(senha)) {
+				 
+				 mensagemExibir("Teste", "Usuário encontrado");	 
+				 
+			 }
+			
+				 
+		 //int i = cursor.getColumnName(2).length();
+		
+	
+		}catch (Exception ex) {
+			mensagemExibir("Atenção!", "Login ou senha incorretos!"+ex.getMessage());
+
+		
+		}
+		return false;
+		
+	}
+	
+	
 	public void abreBanco () {
 		try{
 			String name = "BusUpdb";
-			SQLiteDatabase database = openOrCreateDatabase(name, SQLiteDatabase.CREATE_IF_NECESSARY, null);
+	        database = openOrCreateDatabase(name, SQLiteDatabase.CREATE_IF_NECESSARY, null);
 			mensagemExibir("Teste", "Banco Criado com sucesso");
+			//database.close();
+			
 		
 		}catch(Exception ex) {
 			mensagemExibir("Teste","Erro ao criar banco: "+ex.getMessage());
 			
 		}
+		
+		
+	}
+	
+	public void criaTabelas() {
+        String sql = usuario.criarTabelaUsuario();
+		
+		try {
+			database.execSQL(sql);
+			mensagemExibir("Teste", "Tabela de usuários criada com sucesso");
+			
+			
+		}catch(Exception ex) {
+			mensagemExibir("Comando sql",sql);
+			mensagemExibir("Teste", "Erro ao criar Tabela de usuários: "+ex.getMessage());
+			
+		}
+		
 	}
 	
 	 
